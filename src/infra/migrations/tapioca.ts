0850443c77447cabfbb439673a2bed870a13d726
Pedro Jesus <Pedromra.04@gmail.com>
2024-08-08 21:34:45 -0300

    fix: migration tapioca.ts

--- src/infra/migrations/tapioca.ts
+++ src/infra/migrations/tapioca.ts
@@ -1,14 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Default1723163412495 implements MigrationInterface {
-    name = 'Default1723163412495'
+export class Default1723163648414 implements MigrationInterface {
+    name = 'Default1723163648414'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."items_status_enum" AS ENUM('Disponivel', 'Baixa', 'Em manutenção')`);
+        await queryRunner.query(`ALTER TABLE "items" ADD "status" "public"."items_status_enum" NOT NULL DEFAULT 'Disponivel'`);
         await queryRunner.query(`ALTER TABLE "items" ADD "url" character varying NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN "url"`);
+        await queryRunner.query(`ALTER TABLE "items" DROP COLUMN "status"`);
+        await queryRunner.query(`DROP TYPE "public"."items_status_enum"`);
     }
 
 }
