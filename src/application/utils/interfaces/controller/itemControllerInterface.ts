43ee40779730bf678e5b387d4d83d86e394be50b
Pedro Jesus <pedromra.04@gmail.com>
2024-08-17 11:45:45 -0300

    remove: delete 'interface' folder

--- src/application/utils/interfaces/controller/itemControllerInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export interface IItemController {
-  getItem(req: Request, res: Response): void;
-  getItemByID(req: Request, res: Response): void;
-  createItem(req: Request, res: Response): void;
-  updateItem(req: Request, res: Response): void;
-  deleteItem(req: Request, res: Response): void;
-}
