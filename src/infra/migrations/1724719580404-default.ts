1c4287030917d52a067d53a188d250f656124549
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2024-08-26 22:51:41 -0300

    Update 1724719580404-default.ts

--- src/infra/migrations/1724719580404-default.ts
+++ src/infra/migrations/1724719580404-default.ts
@@ -5,6 +5,7 @@ export class Default1724719580404 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "items" DROP CONSTRAINT "FK_5506ee6f03a1103f62d606a5b25"`);
+        await queryRunner.query(`DROP TABLE "sala"`);
         await queryRunner.query(`CREATE TABLE "salas" ("id" SERIAL NOT NULL, "localizacao" integer NOT NULL, "nome" character varying NOT NULL, "qtd_itens" integer NOT NULL DEFAULT '0', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_cde91c8f48ea30186b7757bd4f8" UNIQUE ("localizacao"), CONSTRAINT "PK_a74948c5a75eb1be20b46c321e6" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "items" ADD "external_id" character varying`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN "data_de_incorporacao"`);
