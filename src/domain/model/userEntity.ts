4b6b66ce53173cd7d79e0c025ce0ffb1fa02873e
Pedro Jesus <pedromra.04@gmail.com>
2024-09-26 21:51:04 -0300

    feat(model): add userEntity

---
+++ src/domain/model/userEntity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { UserRole } from "./enum/roles";
+
+@Entity("usuarios")
+export class User {
+
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  senha: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.USER
+  })
+  role: UserRole;
+}
