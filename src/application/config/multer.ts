1f2257a7ec84870c70bd1194f8dc01fc87018b5d
Pedro Jesus <Pedromra.04@gmail.com>
2024-10-25 19:07:04 -0300

    fix: improve model logic

--- src/application/config/multer.ts
+++ src/application/config/multer.ts
@@ -4,7 +4,10 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
+  fileFilter: (req, file, callback) => {
+    file.originalname = file.originalname.replace(/\s+/g, "_");
+    callback(null, true);
+  },
 });
 
-
-export default upload ;
+export default upload;
