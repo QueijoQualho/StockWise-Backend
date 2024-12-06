233a651733127fdbe80521139806f652034d00ed
Pedro Jesus <pedromra.04@gmail.com>
2024-12-06 17:56:48 -0300

    docs: update swagger

--- src/application/utils/validation/validateDTO.ts
+++ src/application/utils/validation/validateDTO.ts
@@ -6,7 +6,6 @@ import { ItemUpdateDTO, SalaDTO } from "@dto/index";
 import { SignupDTO } from "@dto/user/signupDTO";
 import { LoginDTO } from "@dto/user/loginDTO";
 
-// Função genérica para validar qualquer DTO
 const validateDTO =
   (DTOClass: any) =>
   async (req: Request, res: Response, next: NextFunction) => {
@@ -27,7 +26,6 @@ const validateDTO =
     next();
   };
 
-// Função para formatar os erros de validação
 const formatValidationErrors = (errors: ValidationError[]) => {
   return errors.flatMap((err) =>
     Object.entries(err.constraints || {}).map(([, message]) => ({
@@ -37,7 +35,6 @@ const formatValidationErrors = (errors: ValidationError[]) => {
   );
 };
 
-// Exportando middlewares de validação com base na função genérica
 export const validateCreateSala = validateDTO(SalaDTO);
 export const validateUpdateItem = validateDTO(ItemUpdateDTO);
 export const validateCreateUser = validateDTO(SignupDTO);
